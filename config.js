f53f260d01af1ce6f85be90c84a5c35dc1046ff8
Romana Rashid <romana.rashid02@gmail.com>
2023-09-20 23:58:51 +0200

    database connected

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
 export const PORT=5555;
+export const mongoDBURL="mongodb+srv://gazifahad3:12345@cluster0.rkgbyi5.mongodb.net/?retryWrites=true&w=majority"
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
- import express from "express";
- import { PORT } from "./config.js";
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
- const app=express();
+const app = express();
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("welcome here");
+});
 
- app.listen(PORT,()=>{
-    console.log(`app is running in portb No: ${PORT}`)
- })
- 
+app.listen(PORT, () => {
+  console.log(`app is running in portb No: ${PORT}`);
+});
+mongoose
+  .connect(mongoDBURL)
+  .then((result) => {
+    console.log("app connected succesfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
